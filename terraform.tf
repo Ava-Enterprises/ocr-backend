b1909b4f587c10ed040db731c353a4435338aba2
colinfritz-ai <colinfritz.ai@gmail.com>
2023-12-17 19:34:39 -0600

    adding terraform and deploy actions to dev

---
+++ terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">=0.12.13"
+  backend "s3" {
+    bucket         = "ocr-backend-dev-states"
+    key            = "terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "ocr-backend-dev-state-locking"
+    encrypt        = true
+  }
+}
+
+# Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
+provider "aws" {
+  region  = "us-east-2"
+  version = "~> 2.36.0"
+}
