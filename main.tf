e7fc83bc7731a371fc3dc84c615adc36cfaf610b
colinfritz-ai <colinfritz.ai@gmail.com>
2023-12-20 21:06:49 -0600

    experimenting with unique space

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 resource "aws_instance" "example_server" {
   ami           = "ami-0ee4f2271a4df2d7d"
   instance_type = "t2.micro"
-  name = "${var.env_name}"
+  tags = {
+    Name = "${var.env_name}"
+  }
 }
